c5fa3ec978eb48cf0e2733bb60cdde201d405ff1
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-10 15:11:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     //var articleName= article-one
     //articles[articleName]={} content for object article-one
            var articleName= req.params.articleName;
