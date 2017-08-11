d50cf0c5b25a35be523629ec7e71640bec095393
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-11 20:58:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,12 @@ var element= document.getElementById('main-class');
 element.innerHTML='hello world';
 
 var image=document.getElementById('img');
+var marginLeft='0px';
+var moveRight= function(){
+    marginLeft=maginLeft+10;
+    image.style.marginLeft=marginLeft+'px';
+}
 image.onclick = function(){
-    image.style.marginLeft= '100px';
+    var interval= setInterval(moveRight,100);
     
 };
