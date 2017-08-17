7899af0963d4ca81470067e0e5d2326a9b4fa711
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-17 17:39:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
-var counter=0;
-var button = document.getElementById('counter');
 
+var button = document.getElementById('counter');
+var counter=0;
 button.onclick =function(){
     //request the counter endpoint
     
