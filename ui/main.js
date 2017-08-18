e7a91949e9d9a7228ca06368db2d968c1e4f561b
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-19 01:42:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,13 +27,29 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submitBtn');
 submit.onclick =function(){
-  var names=['name1','name2','name3','name4'];
-  var list=[];
+     var request= new XMLHttpRequest();
+    // store it into a variable
+    request.onreadystatechange= function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(request.status===200)
+            {
+                var names=['name1','name2','name3','name4'];
+                var list=[];
   
-  for(var i=0;i<names.length;i++)
-  {
-      list+='<li>'+names[i]+'</li>';
-  }
-    var ui=document.getElementById('ulist');
-    ui.innerHTML=list;
+                for(var i=0;i<names.length;i++)
+                {
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ui=document.getElementById('ulist');
+                ui.innerHTML=list;
+            }
+        //else ignore
+        }
+    };
+    //make a request
+    request.open('GET','http://sayantanisanyal21.imad.hasura-app.io/counter',true);
+    request.send(null);
+
 }; 
