5192f953fdeb9432efbfa02e39b0dd3248bc7e0b
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-19 01:21:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,9 +81,12 @@ app.get('/:articleName',function(req,res){
   //  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 //});
 
-//app.get('/article-three',function(req,res){
-  //   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-//});
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+ var name=req.params.name;
+    names.push(name);
+     res.send(JSON.stringify(names));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
