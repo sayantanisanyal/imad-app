3e9c6b1a4534c848a561a893f070205d31f4553b
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-22 13:36:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config={
     database:'sayantansanyal21',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:db-sayantanisanyal21-98589
     
 };
 
