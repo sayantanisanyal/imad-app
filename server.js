1df014f19dcabeeaa7e27af3c64ee1a99c5b5196
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-22 14:37:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,14 +128,14 @@ app.get('/articles/:articleName',function(req,res){
     //var articleName= article-one
     //articles[articleName]={} content for object article-one
            
-           pool.query("SELECT * FROM article WHERE title=' "+ req.params.articleName + "'",function(err,result){
+           pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName + "'",function(err,result){
                if(err)
                {
                    res.status(500).send(err.toString());
                }else{ 
                    
                    if(result.rows.length===0){
-                   res.status(404).send("article not found");
+                            res.status(404).send("article not found");
                     }else{
                      var articleData=result.rows[0];               
                      res.send(createTemplate(articleData));
