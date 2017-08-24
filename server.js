210d27f510e6e68188a87a7d83d7fe6a521b3993
sayantanisanyal <sayantanisanyal21@gmail.com>
2017-08-24 17:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,15 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var hash=function(input){
+    var hashed= crypto.pbkdf2Sync(input, 'salt', 100000, 512, 'sha512');
+    return hashed.toString('hex');
+};
+
+app.get('/hash/:input',function(req,res){
+    var hashedString= hash(req.params.input,'this-is-some-string');
+    res.send(hashedString);
+});
 
 var pool= new Pool(config);
 app.get('/test-db',function(req,res){
